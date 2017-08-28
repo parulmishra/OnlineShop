d0cb278a7cbf190d09e6f18e78c04f6efaa65aeb
parul mishra <parul.mishra78@gmail.com>
2017-08-28 12:06:12 -0700

    this commit will add category and product model

--- OnlineShop/Models/Category.cs
+++ OnlineShop/Models/Category.cs
@@ -1,2 +1,187 @@
 using System;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace OnlineShop.Models
+{
+  public class Category
+  {
+    private int _id;
+    private string _name;
+    public Category(string name, int id=0)
+    {
+      _id = id;
+      _name = name;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+
+    public override bool Equals(Object otherCategory)
+    {
+      if (!(otherCategory is Category))
+      {
+        return false;
+      }
+      else
+      {
+        Category newCategory = (Category) otherCategory;
+        bool idEquality = newCategory.GetId() == this._id;
+        bool nameEquality = newCategory.GetName() == this._name;
+        return (idEquality && nameEquality);
+      }
+    }
+    public override int GetHashCode()
+    {
+      return this.GetName().GetHashCode();
+    }
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO categories (name) VALUES (@name);";
+
+      MySqlParameter nameParameter = new MySqlParameter();
+      nameParameter.ParameterName = "@name";
+      nameParameter.Value = _name;
+      cmd.Parameters.Add(nameParameter);
+
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+    public static List<Category> GetAll()
+    {
+      List<Category> allCategories = new List<Category>{};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM categories;";
+
+      var rdr = cmd.ExecuteReader();
+      while(rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        Category newCategory = new Category(name, id);
+        allCategories.Add(newCategory);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allCategories;
+    }
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM categories;";
+
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+    public void Delete()
+    {
+      MySqlConnection conn =DB.Connection();
+      conn.Open();
+
+      MySqlCommand cmd = new MySqlCommand(@"DELETE FROM categories WHERE id=@thisId; DELETE FROM products WHERE category_id =@thisId;",conn);
+
+      MySqlParameter idParameter = new MySqlParameter();
+      idParameter.ParameterName = "@thisId";
+      idParameter.Value = _id;
+      cmd.Parameters.Add(idParameter);
+
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+    public static Category Find(int id)
+    {
+      MySqlConnection conn =DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM categories WHERE id=@thisId";
+
+      MySqlParameter idParameter = new MySqlParameter();
+      idParameter.ParameterName = "@thisId";
+      idParameter.Value = id;
+      cmd.Parameters.Add(idParameter);
+
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+      int idCategory = 0;
+      string name = "";
+
+      while(rdr.Read())
+      {
+        idCategory = rdr.GetInt32(0);
+        name = rdr.GetString(1);
+      }
+      var category = new Category(name, idCategory);
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return category;
+    }
+    public static List<Product> Search(string brand)
+    {
+      List<Category> foundProducts = new List<Product>{};
+      string searchTerm = seachParameter.ToLower()[0].ToString();
+      string wildCard = searchTerm + "%";
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM products WHERE brand LIKE @wildCard;";
+
+      MySqlParameter searchTermParameter = new MySqlParameter();
+      searchTermParameter.ParameterName = "@wildCard";
+      searchTermParameter.Value = wildCard;
+      cmd.Parameters.Add(searchTermParameter);
+
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int productId = rdr.GetInt32(0);
+        int categoryId = rdr.GetInt32(1);
+        string productBrand = rdr.GetString(2);
+        float price = rdr.GetFloat(3);
+        string description = rdr..GetString(4);
+        string seller = rdr.GetString(5);
+        Product newProduct = new Product(categoryId,productBrand,price,description,seller,productId);
+        foundCategories.Add(newProduct);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return foundProducts;
+    }
+   }
+}
