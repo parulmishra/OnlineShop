dccf662d47db9d420bba070ac0756f3aa728be67
Guest User <Guest@epicodus-8.local>
2017-08-29 16:46:45 -0700

    Add webpages and styling

--- OnlineShop/wwwroot/js/scripts.js
+++ OnlineShop/wwwroot/js/scripts.js
@@ -3,54 +3,55 @@ var clickedAmount = 1;
 $(document).ready(function(){
   $("#another_address").click(function(){
     clickedAmount++;
-    $(".add-address").append('<label for ="author">Type of Address</label>'+
+    $(".add-address").append('<label for ="author">Type of Address:</label>'+
     '<div class="form-group">'+
       '<input type="text" class="form-control" name="buyer-address-type' + clickedAmount + '">'+
-    '</div>'+ '<label for ="author">Street</label>' +
+    '</div>'+ '<label for ="author">Street:</label>' +
     '<div class="form-group">' +
       '<input type="text" class="form-control" name="buyer-address-street' + clickedAmount + '">' +
     '</div>' +
-    '<label for ="author">City</label>' +
+    '<label for ="author">City:</label>' +
     '<div class="form-group">' +
       '<input type="text" class="form-control" name="buyer-address-city' + clickedAmount + '">' +
     '</div>' +
-    '<label for ="author">State</label>' +
+    '<label for ="author">State:</label>' +
     '<div class="form-group">' +
       '<input type="text" class="form-control" name="buyer-address-state' + clickedAmount + '">' +
     '</div>' +
-    '<label for ="author">Country</label>' +
+    '<label for ="author">Country:</label>' +
     '<div class="form-group">' +
       '<input type="text" class="form-control" name="buyer-address-country' + clickedAmount + '">' +
     '</div>' +
-    '<label for ="author">Zip</label>' +
+    '<label for ="author">Zip:</label>' +
     '<div class="form-group">' +
       '<input type="text" class="form-control" name="buyer-address-zip' + clickedAmount + '">' +
     '</div>')
   });
   $("#shipping_address").click(function(){
     clickedAmount++;
-    $("#add_new_shipping_address").append('<label for ="author">Type of Address</label>'+
+    $("#add_new_shipping_address").append('<label for ="author">Type of Address:</label>'+
     '<div class="form-group">'+
       '<input type="text" class="form-control" name="buyer-address-type' + clickedAmount + '">'+
-    '</div>'+ '<label for ="author">Street</label>' +
+    '</div>'+ '<label for ="author">Street:</label>' +
     '<div class="form-group">' +
       '<input type="text" class="form-control" name="buyer-address-street' + clickedAmount + '">' +
     '</div>' +
-    '<label for ="author">City</label>' +
+    '<label for ="author">City:</label>' +
     '<div class="form-group">' +
       '<input type="text" class="form-control" name="buyer-address-city' + clickedAmount + '">' +
     '</div>' +
-    '<label for ="author">State</label>' +
+    '<label for ="author">State:</label>' +
     '<div class="form-group">' +
       '<input type="text" class="form-control" name="buyer-address-state' + clickedAmount + '">' +
     '</div>' +
-    '<label for ="author">Country</label>' +
+    '<label for ="author">Country:</label>' +
     '<div class="form-group">' +
       '<input type="text" class="form-control" name="buyer-address-country' + clickedAmount + '">' +
     '</div>' +
-    '<label for ="author">Zip</label>' +
+    '<label for ="author">Zip:</label>' +
     '<div class="form-group">' +
       '<input type="text" class="form-control" name="buyer-address-zip' + clickedAmount + '">' +
     '</div>')
+    $(".address-options").hide();
   });
 });
