92e551a31eb49399745023bb2e188db3bfc5da2f
Guest User <Guest@epicodus-8.local>
2017-08-30 13:57:35 -0700

    add login and additional routing

--- OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/Controllers/HomeController.cs
@@ -6,34 +6,16 @@ namespace OnlineShop.Controllers
 {
   public class HomeController : Controller
   {
-    [HttpPost("/populate")]
+    [HttpGet("/populate")]
     public ActionResult IndexPopulate()
     {
+      Category.DeleteAll();
+      Buyer.DeleteAll();
+
       Populate.PopulateDatabase();
       Dictionary<string, object> model = new Dictionary<string,object>{};
 
-      Buyer buyer = Buyer.Find(0);
-      List<Product> featured = Product.GetAll();
-
-      Random rnd = new Random();
-      List<Product> newFeatured = new List<Product>(){};
-      for(var i = 0; i < 4; i++)
-      {
-        newFeatured.Add(featured[rnd.Next(0, (featured.Count - 1))]);
-      }
-
-      model.Add("categories", Category.GetAll());
-      model.Add("buyer", buyer);
-      model.Add("products", newFeatured);
-
-      return View(model);
-    }
-    [HttpGet("/")]
-    public ActionResult Index()
-    {
-      Dictionary<string, object> model = new Dictionary<string,object>{};
-
-      Buyer buyer = Buyer.Find(0);
+      Buyer buyer = Buyer.Find(-1);
       List<Product> featured = Product.GetAll();
 
       Random rnd = new Random();
@@ -51,8 +33,34 @@ namespace OnlineShop.Controllers
       model.Add("categories", Category.GetAll());
       model.Add("buyer", buyer);
       model.Add("products", newFeatured);
-      
-      return View(model);
+
+      return View("Index", model);
+    }
+    [HttpGet("/{id}")]
+    public ActionResult Index(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string,object>{};
+
+      Buyer buyer = Buyer.Find(id);
+      List<Product> featured = Product.GetAll();
+
+      Random rnd = new Random();
+      List<Product> newFeatured = new List<Product>(){};
+      if(featured.Count > 0)
+      {
+        for(var i = 0; i < 4; i++)
+        {
+          newFeatured.Add(featured[rnd.Next(0, (featured.Count - 1))]);
+        }
+        var featuredProduct = featured[rnd.Next(0, (featured.Count - 1))];
+        model.Add("featured", featuredProduct);
+      }
+
+      model.Add("categories", Category.GetAll());
+      model.Add("buyer", buyer);
+      model.Add("products", newFeatured);
+
+      return View("Index",model);
     }
     [HttpGet("/products/details")]
     public ActionResult ProductDetails()
@@ -69,21 +77,140 @@ namespace OnlineShop.Controllers
     {
         return View();
     }
-    [HttpGet("/cart")]
-    public ActionResult Cart()
+    [HttpGet("/cart/{id}")]
+    public ActionResult Cart(int id)
     {
-        return View();
+      Dictionary<string, object> model = new Dictionary<string,object>{};
+
+      Buyer selectedBuyer = Buyer.Find(id);
+      Order buyerOrder = selectedBuyer.GetCurrentOrder();
+      List<Item> buyerItems = buyerOrder.GetItems();
+
+      model.Add("order", buyerOrder);
+      model.Add("items", buyerItems);
+      // model.Add("products", buyerProducts);
+      model.Add("categories", Category.GetAll());
+      model.Add("buyer", selectedBuyer);
+
+      return View(model);
     }
-    [HttpGet("/order")]
-    public ActionResult Order()
+
+    [HttpPost("/cart/{id}/removed")]
+    public ActionResult CartSingleRemove(int id)
     {
-        return View();
+      Dictionary<string, object> model = new Dictionary<string,object>{};
+
+      Buyer selectedBuyer = Buyer.Find(id);
+      Order buyerOrder = selectedBuyer.GetCurrentOrder();
+
+
+      selectedBuyer.GetCurrentOrder().RemoveItem(int.Parse(Request.Form["item-id"]));
+      List<Item> buyerItems = buyerOrder.GetItems();
+
+      model.Add("order", buyerOrder);
+      model.Add("items", buyerItems);
+      // model.Add("products", buyerProducts);
+      model.Add("categories", Category.GetAll());
+      model.Add("buyer", selectedBuyer);
+
+
+      return View("Cart", model);
     }
+
+    [HttpPost("/cart/{id}/removed/all")]
+    public ActionResult CartRemoveAll(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string,object>{};
+
+      Buyer selectedBuyer = Buyer.Find(id);
+      Order buyerOrder = selectedBuyer.GetCurrentOrder();
+
+
+      selectedBuyer.GetCurrentOrder().RemoveAllItems();
+      List<Item> buyerItems = buyerOrder.GetItems();
+
+      model.Add("order", buyerOrder);
+      model.Add("items", buyerItems);
+      // model.Add("products", buyerProducts);
+      model.Add("categories", Category.GetAll());
+      model.Add("buyer", selectedBuyer);
+
+
+      return View("Cart", model);
+    }
+
+    [HttpPost("/order/{id}")]
+    public ActionResult Order(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string,object>{};
+
+      Buyer selectedBuyer = Buyer.Find(id);
+      Order buyerOrder = selectedBuyer.GetCurrentOrder();
+
+      List<Item> buyerItems = buyerOrder.GetItems();
+
+      model.Add("order", buyerOrder);
+      model.Add("items", buyerItems);
+      // model.Add("products", buyerProducts);
+      model.Add("categories", Category.GetAll());
+      model.Add("buyer", selectedBuyer);
+
+
+      return View(model);
+    }
+
     [HttpGet("/login")]
     public ActionResult LogIn()
     {
-        return View();
+      Dictionary<string, object> model = new Dictionary<string,object>{};
+      Buyer selectedBuyer = Buyer.Find(0);
+      // model.Add("products", buyerProducts);
+      model.Add("categories", Category.GetAll());
+      model.Add("buyer", selectedBuyer);
+      model.Add("login", " ");
+
+      return View(model);
     }
+
+    [HttpPost("/index/login")]
+    public ActionResult IndexLoggedIn(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string,object>{};
+      string buyerPassword = Request.Form["buyer-password"];
+      string buyerName = Request.Form["buyer-name"];
+
+      Buyer selectedBuyer = Buyer.TryLogin(buyerName,buyerPassword);
+
+      // model.Add("products", buyerProducts);
+      model.Add("categories", Category.GetAll());
+      model.Add("buyer", selectedBuyer);
+      Console.WriteLine(selectedBuyer.GetId());
+      //try login
+      if (selectedBuyer.GetId() == 0)
+      {
+        string loginFail = "User name/password not found";
+        model.Add("login", loginFail);
+        return View("login",model);
+      }
+      else{ // return to index logged in
+        Random rnd = new Random();
+        List<Product> featured = Product.GetAll();
+        List<Product> newFeatured = new List<Product>(){};
+        if(featured.Count > 0)
+        {
+          for(var i = 0; i < 4; i++)
+          {
+            newFeatured.Add(featured[rnd.Next(0, (featured.Count - 1))]);
+          }
+          var featuredProduct = featured[rnd.Next(0, (featured.Count - 1))];
+          model.Add("featured", featuredProduct);
+        }
+        model.Add("products", newFeatured);
+        return View("Index", model);
+      }
+
+    }
+
     [HttpGet("/detail")]
     public ActionResult BuyerDetail()
     {
