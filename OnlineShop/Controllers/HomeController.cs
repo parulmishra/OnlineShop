ecd5ae2031c50ef9d17379b6b130ee9d8f50abb6
Guest User <Guest@epicodus-8.local>
2017-08-30 10:18:15 -0700

    Add index HC route

--- OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/Controllers/HomeController.cs
@@ -6,10 +6,53 @@ namespace OnlineShop.Controllers
 {
   public class HomeController : Controller
   {
+    [HttpPost("/populate")]
+    public ActionResult IndexPopulate()
+    {
+      Populate.PopulateDatabase();
+      Dictionary<string, object> model = new Dictionary<string,object>{};
+
+      Buyer buyer = Buyer.Find(0);
+      List<Product> featured = Product.GetAll();
+
+      Random rnd = new Random();
+      List<Product> newFeatured = new List<Product>(){};
+      for(var i = 0; i < 4; i++)
+      {
+        newFeatured.Add(featured[rnd.Next(0, (featured.Count - 1))]);
+      }
+
+      model.Add("categories", Category.GetAll());
+      model.Add("buyer", buyer);
+      model.Add("products", newFeatured);
+
+      return View(model);
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
-        return View();
+      Dictionary<string, object> model = new Dictionary<string,object>{};
+
+      Buyer buyer = Buyer.Find(0);
+      List<Product> featured = Product.GetAll();
+
+      Random rnd = new Random();
+      List<Product> newFeatured = new List<Product>(){};
+      if(featured.Count > 0)
+      {
+        for(var i = 0; i < 4; i++)
+        {
+          newFeatured.Add(featured[rnd.Next(0, (featured.Count - 1))]);
+        }
+        var featuredProduct = featured[rnd.Next(0, (featured.Count - 1))];
+        model.Add("featured", featuredProduct);
+      }
+
+      model.Add("categories", Category.GetAll());
+      model.Add("buyer", buyer);
+      model.Add("products", newFeatured);
+      
+      return View(model);
     }
     [HttpGet("/products/details")]
     public ActionResult ProductDetails()
@@ -17,9 +60,9 @@ namespace OnlineShop.Controllers
         return View();
     }
     [HttpGet("/category")]
-    public ActionResult Category()
+    public ActionResult CategoryView()
     {
-        return View();
+        return View("Category");
     }
     [HttpGet("/buyerform/add")]
     public ActionResult BuyerForm()
